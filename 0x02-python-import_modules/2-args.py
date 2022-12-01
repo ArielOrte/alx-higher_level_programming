6596877c8b796e0d398e58b02526689c38bc3e89
Ariel <arielchrisortega@gmail.com>
2022-12-01 10:36:57 -0800

    a function that creates a copy of the string, removing the character at the position n (not the Python way, the “C array index”).

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argv = sys.argv[1:]
-    argv_c = len(argv)
-    i = 0
-    if argv_c is 0:
+    argv = sys.argv
+    argv_c = len(argv) - 1
+    i = 1
+    if argv_c == 0:
         print("{:d} arguments.".format(argv_c))
-    elif argv_c is 1:
+    elif argv_c == 1:
         print("{:d} argument:".format(argv_c))
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+        print("{:d}: {:s}".format(i, argv[i]))
     else:
         print("{:d} arguments:".format(argv_c))
         while i <= argv_c:
-            print("{:d}: {:s}".format(i, sys.argv[i]))
+            print("{:d}: {:s}".format(i, argv[i]))
             i += 1
