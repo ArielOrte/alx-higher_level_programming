44e14685c88203dfecebb158cad21e63368b869e
Ariel <arielchrisortega@gmail.com>
2022-12-19 10:31:46 -0800

    8. Safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except ZeroDivisionError as zde:
+        sys.stderr.write("Exception: {}\n".format(zde.args[0]))
+    except IndexError as ie:
+        sys.stderr.write("Exception: {}\n".format(ie.args[0]))
+    except:
+        raise
+        return None
