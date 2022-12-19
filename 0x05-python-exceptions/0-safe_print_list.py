a7b51dfa484cb981fd47f1b3208263e8cf5d0a1c
Ariel <arielchrisortega@gmail.com>
2022-12-19 07:49:16 -0800

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,5 +8,7 @@ def safe_print_list(my_list=[], x=0):
             count += 1
     except IndexError:
         print("index of a sequence is out of range")
+        return count
     else:
+        print("")
         return count
