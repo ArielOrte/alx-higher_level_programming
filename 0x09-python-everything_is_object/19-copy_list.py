ae3a262266b3fdac497c2f99bbc4b2bdfef0dec6
Ariel <arielchrisortega@gmail.com>
2023-01-04 10:53:47 -0800

    19-copy_list.py

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-"""Defines the copy list function."""
-
-
 def copy_list(l):
-    """Creates a copy of the list.
-
-    Args:
-        l: List to be copied.
-    Returns:
-        The copy of the created list
-    """
         return list(l)
