fb367f8fd8598611bd5f56bb8fddf34bd26e7a1e
Ariel <arielchrisortega@gmail.com>
2023-01-10 02:28:53 -0800

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+
+Module with read_file.
+
+"""
+
+
 def read_file(filename=""):
-    """function that reads a text file and prints it"""
+    """Function that reads a text file and prints it."""
 
     with open(filename) as f:
         text = f.read()
